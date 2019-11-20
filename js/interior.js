663b6f8cb35a8f40ff6a2446ad520a27aacc140c
Igor <Kankovigor@gmail.com>
2019-11-20 06:20:14 +0000

    css almost finished, tried bootstrap

---
+++ js/interior.js
@@ -0,0 +1,12 @@
+document.querySelector('.get-variant').addEventListener('click', getVariant);
+
+function getVariant() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.variants').innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open('get', 'variant.html', true);
+    xhr.send();
+}
